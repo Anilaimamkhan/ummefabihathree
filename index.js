8bee7655a32cb69dea69d72b74317fd27c787e6a
Anilaimamkhan <124345160+Anilaimamkhan@users.noreply.github.com>
2023-02-09 18:27:09 +0500

    index

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("hello");
+// let bars = document.querySelector("#bars");
+// let nav = document.querySelector("nav");
+
+// bars.addEventListener("click", () => {
+// // alert("me");
+// bars.classList.toggle("active");
+// nav.classList.toggle("active");
+// });
